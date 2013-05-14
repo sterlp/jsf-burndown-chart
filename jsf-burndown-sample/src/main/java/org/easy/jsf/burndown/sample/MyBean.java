1214b7a6d1866f9b2c3264b5175b892825163d3c
Paul Sterl <puele@hotmail.com>
2013-05-14 16:51:45 +0200

    fixed start date

--- jsf-burndown-sample/src/main/java/org/easy/jsf/burndown/sample/MyBean.java
+++ jsf-burndown-sample/src/main/java/org/easy/jsf/burndown/sample/MyBean.java
@@ -21,13 +21,13 @@ public class MyBean implements Serializable {
                 new LocalDate(2013, 5, 1), 
                 new LocalDate(2013, 5, 31), 250);
         
-        burndown.addDay(new LocalDate(2013, 5, 1), 8, "First Day");
-        burndown.addDay(new LocalDate(2013, 5, 2), -16, "Unplanned Bug");
-        burndown.addDay(new LocalDate(2013, 5, 2), 40, null);
-        burndown.addDay(new LocalDate(2013, 5, 3), 12, null);
-        burndown.addDay(new LocalDate(2013, 5, 6), 33, null);
-        burndown.addDay(new LocalDate(2013, 5, 7), -44, "Added a new small story");
-        burndown.addDay(new LocalDate(2013, 5, 7), 24, null);
+        burndown.addDay(new LocalDate(2013, 5, 2), 8, "First Day");
+        burndown.addDay(new LocalDate(2013, 5, 3), -16, "Unplanned Bug");
+        burndown.addDay(new LocalDate(2013, 5, 3), 40, null);
+        burndown.addDay(new LocalDate(2013, 5, 4), 12, null);
+        burndown.addDay(new LocalDate(2013, 5, 7), 33, null);
+        burndown.addDay(new LocalDate(2013, 5, 8), -44, "Added a new small story");
+        burndown.addDay(new LocalDate(2013, 5, 8), 24, null);
     }
 
     public IterationBurndown getBurndown() {
